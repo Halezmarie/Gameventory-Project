d51365c40887dc202eb4fa2c996f188ba9651258
halezmarie <teacherhaleymarie@gmail.com>
2021-03-22 14:40:56 -0500

    made authenication look more DRY

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
 
+  get '/login' do
+    erb :login
+  end
+  
+  post '/login' do
+     @user = User.find_by(:email => params[:email])
+     if @user.authenticate(params[:password])
+       session[:user_id] = @user.id 
+       redirect to "/users/#{@user.id}"
+     else
+       flash[:message] = "Oh no! Try again!"
+       redirect to '/login'
+     end
+   end
+  
+    get '/users/:id' do
+      @game_items = current_user.game_items
+      erb :'users/show'
+    end
+  
+    get '/logout' do
+      session.clear
+      redirect '/'
+    end
+  end
+  
+
 get '/signup' do
        erb :'users/signup'
     end
@@ -22,28 +49,3 @@ end
 
 
 
-get '/login' do
-  erb :login
-end
-
-post '/login' do
-   @user = User.find_by(:email => params[:email])
-   if @user && @user.authenticate(params[:password])
-     session[:user_id] = @user.id 
-     redirect to "/users/#{@user.id}"
-   else
-     flash[:message] = "Oh no! Try again!"
-     redirect to '/login'
-   end
- end
-
-  get '/users/:id' do
-    @game_items = current_user.game_items
-    erb :'users/show'
-  end
-
-  get '/logout' do
-    session.clear
-    redirect '/'
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
     icarus= User.create(name: "Icarus", email: "icarus@icarus.com", password: "password")
     anastasia= User.create(name: "Anastasia", email: "anastasia@anastasia.com", password: "password")
 
-GameItem.create(title: "Mortal Kombat", genre: "Fighting", url: "https://happymag.tv/wp-content/uploads/2020/03/mortal-kombat-2.jpg", rating: "M", user_id: icarus.id)
+GameItem.create(title: "Mortal Kombat PS1", genre: "Fighting", url: "https://happymag.tv/wp-content/uploads/2020/03/mortal-kombat-2.jpg", rating: "M", user_id: icarus.id)
 
-GameItem.create(title: "Final Fantasy 7", genre: "Role-Playing", url: "https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg", rating: "T", user_id: anastasia.id)
+GameItem.create(title: "Final Fantasy 7 PS1", genre: "Role-Playing", url: "https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg", rating: "T", user_id: anastasia.id)
 

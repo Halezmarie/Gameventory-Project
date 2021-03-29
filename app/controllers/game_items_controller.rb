5275741e71c09f959a19377ed54e2593f1955574
halezmarie <teacherhaleymarie@gmail.com>
2021-03-29 01:19:43 -0500

    creating a game wouldnt work, put search bar elsewhere

--- app/controllers/game_items_controller.rb
+++ app/controllers/game_items_controller.rb
@@ -1,4 +1,5 @@
 class GameItemsController < ApplicationController
+ 
 
   get '/game_items' do
     @game_items = GameItem.all
@@ -10,29 +11,28 @@ class GameItemsController < ApplicationController
   end
 
   post '/game_items' do
-      redirect_if_not_logged_in
     if params[:title] != ""
-        @game_item = GameItem.create(title: params[:title], genre: params[:genre], url: params[:url], rating: params[:rating], user_id: current_user.id)
-        flash[:message] = "Woohoo, your game has been created without any problems!"
-        redirect to "/game_items/#{@game_item.id}"
+      @game_item = GameItem.create(title: params[:title], genre: params[:genre], url: params[:url], rating: params[:rating], user_id: current_user.id)
+      flash[:message] = "Woohoo, your game has been created without any problems!"
+      redirect to "/game_items/#{@game_item.id}"
     else
       redirect to '/game_items/new'
+    end
   end
-end
 
 
   get '/game_items/:id' do
     redirect_if_not_logged_in
     @game_item = GameItem.find_by(id: params[:id])
-  erb :'game_items/show'
+    erb :'game_items/show'
   end
 
   get '/game_items/:id/edit' do 
     redirect_if_not_logged_in
     @game_item = GameItem.find_by(id: params[:id])
     if @game_item.user == current_user
-        erb :'/game_items/edit'
-      else
+      erb :'/game_items/edit'
+    else
       redirect "users/#{current_user.id}"
     end
   end
@@ -52,7 +52,7 @@ end
 
   delete '/game_items/:id/delete' do
     @game_item = GameItem.find_by(id: params[:id])
-      if logged_in? && @game_item.user.id == current_user.id
+    if logged_in? && @game_item.user.id == current_user.id
       @game_item.destroy
       flash[:message] = "You deleted the game without any problems!"
       redirect '/game_items'
@@ -62,11 +62,12 @@ end
   end
 
 
-post '/search' do 
-  @game_item = GameItem.find_by(title: params[:game_items][:title])
-  if @game_item 
-    erb :'/game_items/result'
-   redirect '/game_items'
-end
-end
+  post '/search' do 
+    @game_item = GameItem.find_by(title: params[:game_items][:title])
+    if @game_item 
+      erb :'/game_items/result'
+    else
+      redirect '/game_items'
+    end
+  end
 end
